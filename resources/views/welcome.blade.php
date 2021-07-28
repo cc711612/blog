c5ae5dbf74d50548926eefd7884f4c3a8bd5926e
Roy <cc711612@gmail.com>
2021-07-28 16:48:44 +0800

    new test websocket

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>Laravel WebSocket 示例</title>
+
+</head>
+
+<body>
+
+<div id="div-data">
+    <table class="table table-striped table-bordered table-hover" id="sample_1" name="dataTable">
+        <thead>
+        <tr class="text-center-row">
+            <th> 名稱</th>
+            <th> Email</th>
+            <th> 其他操作</th>
+        </tr>
+        </thead>
+        <tbody id="div-body">
+        @foreach($Users as $element)
+            <tr class="text-center-row">
+                <td>{{$element->name}}</td>
+                <td>{{$element->email}}</td>
+                <td>
+                    <a href="#" class="btn dark  btn-outline sbold uppercase  ">
+                        <i class="fa fa-edit"></i> 編輯部門名稱 </a>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/3.0.0/mustache.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script type="text/x-mustache" id="template">
+<tr class="text-center-row">
+        <td>@{{ name }}</td>
+        <td>@{{ email }}</td>
+        <td>
+            <a href="#" class="btn dark  btn-outline sbold uppercase  ">
+                <i class="fa fa-edit"></i> 編輯部門名稱 </a>
+        </td>
+</tr>
+
+</script>
+<script>
+    window.Echo.channel('EventTriggered')
+        .listen('GetRequestEvent', (e) => {
+            let template = $("#template").html();
+            document.querySelector('#div-body').innerHTML += Mustache.render(template, {
+                name: e.message.name,
+                email: e.message.email
+            });
+        })
+</script>
+
+</body>
+
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $Users = (new \App\Models\User())->all();
+
+    return view('welcome',compact('Users'));
+});
+Route::get('/trigger/{data}', function ($data) {
+    echo "<p>You have sent $data</p>";
+    event(new App\Events\GetRequestEvent($data));
+});
