6f4e18d2b8a9bc43016575103c054e0705c94f1b
Roy <cc711612@gmail.com>
2021-09-04 14:17:00 +0800

    更換auth login 照片

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,29 +28,16 @@
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required
                              autocomplete="current-password"/>
             </div>
-
-            <div class="block mt-4">
-                {{--                <label for="remember_me" class="flex items-center">--}}
-                {{--                    <x-jet-checkbox id="remember_me" name="remember"/>--}}
-                {{--                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>--}}
-                {{--                </label>--}}
-                @if (Route::has('password.request'))
-                    <div style="text-align: left">
-                        <a class="underline text-sm text-gray-600 hover:text-gray-900"
-                           href="{{ route('password.request') }}">
-                            {{ __('Forgot your password?') }}
-                        </a>
-                    </div>
-                @endif
-            </div>
             <div class="flex items-center justify-end mt-4">
                 <div class="facebook_login ml-4">
-                    <a href="{{route('social.facebook.login')}}" title="facebook登入"><i
-                            class="fab fa-2x fa-facebook-square"></i></a>
+                    <a href="{{route('social.facebook.login')}}" title="facebook登入">
+                        <img src="{{asset('assets/img/auth/btn_facebook_login.png')}}" style="width: 7rem">
+                    </a>
                 </div>
                 <div class="line_login ml-4">
-                    <a href="{{route('social.line.login')}}" title="Line登入"><i
-                            class="fab fa-2x fa-line text-success mr-1"></i></a>
+                    <a href="{{route('social.line.login')}}" title="Line登入">
+                        <img src="{{asset('assets/img/auth/btn_line_login.png')}}" style="width: 7rem">
+                    </a>
                 </div>
                 <button type="button" id="login"
                         class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition ml-4">
