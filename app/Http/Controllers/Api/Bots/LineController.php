2371ef5270616409583069424242d58fb6c8172a
Roy <cc711612@gmail.com>
2022-03-09 15:54:36 +0800

    暫存

--- app/Http/Controllers/Api/Bots/LineController.php
+++ app/Http/Controllers/Api/Bots/LineController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Log;
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
 
 class LineController extends BaseController
@@ -14,7 +16,21 @@ class LineController extends BaseController
 
     public function test(Request $request){
         Log::channel()->info(json_encode($request->toArray(), JSON_UNESCAPED_UNICODE));
+        //實體化linebot物件
+        $httpClient = new CurlHTTPClient(env('LINEBOT_TOKEN'));
+        $bot = new LINEBot($httpClient, ['channelSecret' => env('LINEBOT_SECRET')]);
 
+        //取得使用者id和訊息內容
+        $text = $request->events[0]['message']['text'];
+        $user_id = $request->events[0]['source']['userId'];
+
+        //透過dialogFlow判斷訊息意圖
+        $dialog = $this->dialog($text);
+        $noLimit = !strpos($dialog->content(), 'Vague response');
+
+        //將以上拿到的資訊寫進log裡，debug用
+        Log::info($text);
+        Log::debug($dialog->content());
         return response('',200);
     }
 }
