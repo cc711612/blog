6237efb7fcd8efdbd90b8212228619c0b5b4ef33
Roy <cc711612@gmail.com>
2022-03-16 12:57:54 +0800

    新增留言line通知

--- app/Http/Controllers/Api/Comments/CommentController.php
+++ app/Http/Controllers/Api/Comments/CommentController.php
@@ -17,6 +17,10 @@ use App\Http\Requesters\Api\Comments\CommentUpdateRequest;
 use App\Http\Validators\Api\Comments\CommentUpdateValidator;
 use App\Http\Requesters\Api\Comments\CommentDestroyRequest;
 use App\Http\Validators\Api\Comments\CommentDestroyValidator;
+use App\Models\Services\Web\ArticleWebService;
+use App\Models\Services\ArticleService;
+use App\Jobs\sendLineMessage;
+use Illuminate\Support\Facades\Log;
 
 class CommentController extends BaseController
 {
@@ -33,10 +37,10 @@ class CommentController extends BaseController
         $Validate = (new CommentIndexValidator($Requester))->validate();
         if ($Validate->fails() === true) {
             return response()->json([
-                'status' => false,
-                'code' => 400,
-                'message' => $Validate->errors(),
-                'data' => [],
+                'status'   => false,
+                'code'     => 400,
+                'message'  => $Validate->errors(),
+                'data'     => [],
                 'redirect' => '',
             ]);
         }
@@ -46,20 +50,20 @@ class CommentController extends BaseController
             ->getCommentsByArticleId();
 
         return response()->json([
-            'status' => true,
-            'code' => 200,
+            'status'  => true,
+            'code'    => 200,
             'message' => [],
-            'data' => $Comments->map(function ($comment) {
+            'data'    => $Comments->map(function ($comment) {
                 return [
-                    'id' => Arr::get($comment, 'id'),
-                    'user'=>[
-                        'id' => Arr::get($comment, 'users.id'),
-                        'name' => Arr::get($comment, 'users.name'),
+                    'id'         => Arr::get($comment, 'id'),
+                    'user'       => [
+                        'id'    => Arr::get($comment, 'users.id'),
+                        'name'  => Arr::get($comment, 'users.name'),
                         'image' => Arr::get($comment, 'users.images.cover', $this->getDefaultImage()),
                     ],
-                    'content' => Arr::get($comment, 'content'),
+                    'content'    => Arr::get($comment, 'content'),
                     'updated_at' => Arr::get($comment, 'updated_at')->format('Y-m-d H:i:s'),
-                    'logs' => Arr::get($comment, 'logs', []),
+                    'logs'       => Arr::get($comment, 'logs', []),
                 ];
             }),
         ]);
@@ -67,7 +71,7 @@ class CommentController extends BaseController
 
 
     /**
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      *
      * @return \Illuminate\Http\JsonResponse
      * @Author: Roy
@@ -81,25 +85,49 @@ class CommentController extends BaseController
         $Validate = (new CommentStoreValidator($Requester))->validate();
         if ($Validate->fails() === true) {
             return response()->json([
-                'status' => false,
-                'code' => 400,
-                'message' => $Validate->errors(),
-                'data' => [],
+                'status'   => false,
+                'code'     => 400,
+                'message'  => $Validate->errors(),
+                'data'     => [],
                 'redirect' => '',
             ]);
         }
-        #Create
-        $Entity = (new CommentEntity())->create(Arr::get($Requester, CommentEntity::Table));
+        try {
+            #Create
+            $Entity = (new CommentEntity())->create(Arr::get($Requester, CommentEntity::Table));
+            # 傳送LINE給user
+            $ArticleUser = (new ArticleService())->getArticleUserSocialByArticleId(Arr::get($Requester, 'id'));
+            if (is_null($ArticleUser) === false && is_null(Arr::get($ArticleUser,
+                    'users.socials.0')) === false && is_null($Entity) === false) {
+                sendLineMessage::dispatch(
+                    [
+                        'user_id' => Arr::get($ArticleUser, 'users.socials.0.social_type_value'),
+                        'message' => sprintf("此文章有新留言~\n%s\n留言內容為:\n%s",
+                            route("article.show", ['article' => Arr::get($Requester, 'id')]), $Entity->content),
+                    ]
+                );
+            }
+        } catch (\Exception $exception) {
+            Log::channel()->error(sprintf("%s error params : %s", get_class($this),
+                json_encode($exception, JSON_UNESCAPED_UNICODE)));
+            return response()->json([
+                'status'  => false,
+                'code'    => 500,
+                'message' => [],
+                'data'    => [],
+            ]);
+        }
+
         return response()->json([
-            'status' => true,
-            'code' => 200,
+            'status'  => true,
+            'code'    => 200,
             'message' => [],
-            'data' => [],
+            'data'    => [],
         ]);
     }
 
     /**
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      *
      * @return \Illuminate\Http\JsonResponse
      * @Author: Roy
@@ -112,34 +140,34 @@ class CommentController extends BaseController
         $Validate = (new CommentUpdateValidator($Requester))->validate();
         if ($Validate->fails() === true) {
             return response()->json([
-                'status' => false,
-                'code' => 400,
-                'message' => $Validate->errors(),
-                'data' => [],
+                'status'   => false,
+                'code'     => 400,
+                'message'  => $Validate->errors(),
+                'data'     => [],
                 'redirect' => '',
             ]);
         }
         $Entity = (new CommentService())->setRequest($Requester->toArray())->update();
         if ($Entity) {
             return response()->json([
-                'status' => true,
-                'code' => 200,
-                'message' => [],
-                'data' => [],
+                'status'   => true,
+                'code'     => 200,
+                'message'  => [],
+                'data'     => [],
                 'redirect' => route('article.show', ['article' => Arr::get($Requester, 'article_id')]),
             ]);
         }
         return response()->json([
-            'status' => false,
-            'code' => 400,
-            'message' => ['error' => '系統異常,請重新整理'],
-            'data' => [],
+            'status'   => false,
+            'code'     => 400,
+            'message'  => ['error' => '系統異常,請重新整理'],
+            'data'     => [],
             'redirect' => '',
         ]);
     }
 
     /**
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      *
      * @Author: Roy
      * @DateTime: 2021/7/30 下午 02:13
@@ -150,10 +178,10 @@ class CommentController extends BaseController
         $Validate = (new CommentDestroyValidator($Requester))->validate();
         if ($Validate->fails() === true) {
             return response()->json([
-                'status' => false,
-                'code' => 400,
+                'status'  => false,
+                'code'    => 400,
                 'message' => $Validate->errors(),
-                'data' => [],
+                'data'    => [],
             ]);
         }
         #刪除
@@ -162,19 +190,20 @@ class CommentController extends BaseController
                 ->delete()
             ) === false) {
             return response()->json([
-                'status' => true,
-                'code' => 200,
+                'status'  => true,
+                'code'    => 200,
                 'message' => [],
-                'data' => [],
+                'data'    => [],
             ]);
         }
         return response()->json([
-            'status' => false,
-            'code' => 400,
+            'status'  => false,
+            'code'    => 400,
             'message' => ['error' => '系統異常'],
-            'data' => [],
+            'data'    => [],
         ]);
     }
+
     /**
      * @return string
      * @Author: Roy
