1ce57712a9788acc2972d9fda1160f445081bdc0
Roy <cc711612@gmail.com>
2022-08-05 15:36:25 +0800

    刪除多餘blade

--- resources/views/blog/main.blade.php
+++ resources/views/blog/main.blade.php
@@ -26,36 +26,18 @@
     -->
     {{ seo()->render() }}
     <link rel="icon" type="image/x-icon" href="{{secure_url('/favicon.ico')}}"/>
-    <!-- Font Awesome icons (free version)-->
-    <!-- Google fonts-->
-{{--    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet"--}}
-{{--          type="text/css"/>--}}
-{{--    <link--}}
-{{--        href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800"--}}
-{{--        rel="stylesheet" type="text/css"/>--}}
 <!-- Core theme CSS (includes Bootstrap)-->
     <link href="{{secure_url('/css/styles.css?v='.config('app.version'))}}" rel="stylesheet"/>
     <link href="{{secure_url('/css/main.css?v='.config('app.version'))}}" rel="stylesheet"/>
     <link href="{{secure_url('/css/badge.css?v='.config('app.version'))}}" rel="stylesheet"/>
     <!-- BEGIN PAGE LEVEL PLUGINS -->
-@stack('css-plugins')
-<!-- END PAGE LEVEL PLUGINS -->
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-NNCGJ1VG5L"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-
-        gtag('js', new Date());
-
-        gtag('config', 'G-NNCGJ1VG5L');
-    </script>
+    @stack('css-plugins')
+    <!-- END PAGE LEVEL PLUGINS -->
+    @include("layouts.tracking_header")
     @laravelPWA
 </head>
 <body>
+@include("layouts.tracking_noscript")
 <!-- Messenger 洽談外掛程式 Code -->
 <div id="fb-root"></div>
 <!-- Your 洽談外掛程式 code -->
@@ -65,36 +47,7 @@
     <img src="{{asset('/assets/img/loader.gif')}}" alt="Loading..."/>
 </div>
 <!-- Navigation-->
-<nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
-    <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="{{route('website.index')}}">{{ config('app.name', 'Laravel') }}</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            Menu
-            <i class="fas fa-bars"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ms-auto py-4 py-lg-0">
-                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="{{route('website.index')}}">Home</a>
-                </li>
-                @if(is_null(\Illuminate\Support\Facades\Auth::user()))
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="{{route('login')}}">LOGIN</a>
-                    </li>
-                @else
-                    @if(in_array(\Illuminate\Support\Facades\Auth::id(),config('admin.user_ids')))
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="{{route('admin.home')}}">Admin</a>
-                        </li>
-                    @endif
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="{{route('article.create')}}">POST</a>
-                    </li>
-
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" id="logout" href="/logout">LOGOUT</a>
-                    </li>
-                @endif
-            </ul>
-        </div>
-    </div>
-</nav>
+@include("layouts.header")
 <div class="number-of-people">
     <div class="badge" id="online">
         0
@@ -113,45 +66,9 @@
               fill="white"/>
     </svg>
 </div>
-
 @yield("content")
 <!-- Footer-->
-<footer class="border-top">
-    <div class="container px-4 px-lg-5">
-        <div class="row gx-4 gx-lg-5 justify-content-center">
-            <div class="col-md-10 col-lg-8 col-xl-7">
-                <ul class="list-inline text-center">
-                    <li class="list-inline-item">
-                        <a href="https://www.instagram.com/comman24/" alt="instagram" target="_blank">
-                            <span class="fa-stack fa-lg">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-instagram fa-stack-1x fa-inverse"></i>
-                            </span>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="https://www.facebook.com/roy22887" alt="facebook" target="_blank">
-                            <span class="fa-stack fa-lg">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                            </span>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="https://github.com/cc711612" alt="github" target="_blank">
-                            <span class="fa-stack fa-lg">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-github fa-stack-1x fa-inverse"></i>
-                            </span>
-                        </a>
-                    </li>
-                </ul>
-                <div class="small text-center text-muted fst-italic">Copyright &copy; 2021 Roy<p
-                        id="busuanzi_value_site_pv"></p></div>
-            </div>
-        </div>
-    </div>
-</footer>
+@include("layouts.footer")
 <script>
     let logout_uri = '{{route('logout')}}';
     // 線上人數
