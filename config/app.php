bf14b959d2ebde44470590861111058714f2e712
Roy <cc711612@gmail.com>
2022-08-06 11:33:04 +0800

    修改參數

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name'      => env('APP_NAME', 'Laravel'),
+    'name'      => env('APP_NAME', 'Blog'),
     'sub_title' => env('APP_SUB_TITLE', 'Record'),
     /*
     |--------------------------------------------------------------------------
