143abb90577d12ff3bd16fd827b9e09f9020672f
Roy <cc711612@gmail.com>
2023-02-16 21:58:12 +0800

    envoy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,5 +1,5 @@
 {{--定義server的名稱 使用者@deploy-unit的內部IP--}}
-@servers(['main' => 'gitlab@172.31.42.248'])
+@servers(['main' => 'gitlab-runner@172.31.42.248'])
 
 {{--自動化腳本執行內容--}}
 @task('deploy', ['on' => 'main'])
