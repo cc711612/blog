47cdd5cb9078ad0ed710b4305b5242a033b307ec
Roy <cc711612@gmail.com>
2024-10-18 23:32:47 +0800

    add telescope

--- config/app.php
+++ config/app.php
@@ -186,6 +186,7 @@ return [
         App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
         \SocialiteProviders\Manager\ServiceProvider::class,
